0ece0a0ac1ba799b4b51bc8251f8983b976b7e53
Carla Holcomb <carla.programmer@gmail.com>
2020-06-10 19:45:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function fetchBooks() {
+  
+      fetch('http://api.open-notify.org/astros.json')
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      console.log(json);
+      console.log(`Holy cow! There are ${json["number"]} humans in space.`);
+    });
 
 }
 
